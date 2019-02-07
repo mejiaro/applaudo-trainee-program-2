179212e0d45b48f26f08571b86928f5a61d2a857
Carlos Medrano <cemg_neto@hotmail.com>
2019-02-07 10:00:22 -0600

    more identation

--- Week1/Ejer2.rb
+++ Week1/Ejer2.rb
@@ -1,61 +1,61 @@
 class Array
- def to_histogram()
-   b = self
-   if(b.length==0)
-     puts "The array is empty"
-   else
-     puts b.each_with_object(Hash.new(0)) {|word, hash| hash[word] += 1}
-   end
- end
+  def to_histogram
+    b = self
+    if b.empty?
+      puts 'The array is empty'
+    else
+      puts b.each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
+    end
+  end
 end
 
-arr=[]
-op=0
-36.times{ print "-"}
+arr = []
+op = 0
+36.times { print '-' }
 puts "\n|              Problem 3           |"
-while op.to_i!=5 do
-  36.times{ print "-"}
+while op.to_i != 5
+  36.times { print '-' }
   puts "\n|               Menu               |"
-  36.times{ print "-"}
+  36.times { print '-' }
   puts "\n| 1.Add number to array            |"
-  puts "| 2.Print the array                |"
-  puts "| 3.Calculate histogram            |"
-  puts "| 4.Clear the array                |"
-  puts "| 5.Exit                           |"
-  36.times{ print "-"}
+  puts '| 2.Print the array                |'
+  puts '| 3.Calculate histogram            |'
+  puts '| 4.Clear the array                |'
+  puts '| 5.Exit                           |'
+  36.times { print '-' }
   puts "\nType the number of the option:"
-  op= gets.chomp
+  op = gets.chomp
   case op.to_i
-    when 1
-      puts "Type the number you like to add to the array"
-      num=gets.chomp
-      begin
-        num = Integer(num)
-        arr << num
-        puts "Number added correctly"
-      rescue ArgumentError, TypeError
-        puts "Type a valid number"
-      end
-    when 2
-      if arr.length==0
-        puts "The array is empty"
-      else
-        puts "The array is: #{arr}"
-      end
-    when 3
-      arr.to_histogram
-    when 4
-      puts "Are you sure you want to clear the array? (S/N)"
-      q=gets.chomp
-      if(q.downcase=="s")
-        arr.clear
-        puts "Array cleared"
-      else
-        puts "The array was not cleared"
-      end
-    when 5
-      puts "Leaving the program..."
+  when 1
+    puts 'Type the number you like to add to the array'
+    num = gets.chomp
+    begin
+      num = Integer(num)
+      arr << num
+      puts 'Number added correctly'
+    rescue ArgumentError, TypeError
+      puts 'Type a valid number'
+    end
+  when 2
+    if arr.empty?
+      puts 'The array is empty'
     else
-      puts "The option is not valid"
+      puts "The array is: #{arr}"
+    end
+  when 3
+    arr.to_histogram
+  when 4
+    puts 'Are you sure you want to clear the array? (S/N)'
+    q = gets.chomp
+    if q.casecmp('s').zero?
+      arr.clear
+      puts 'Array cleared'
+    else
+      puts 'The array was not cleared'
+    end
+  when 5
+    puts 'Leaving the program...'
+  else
+    puts 'The option is not valid'
   end
 end
--- Week1/Ejer3.rb
+++ Week1/Ejer3.rb
@@ -1,95 +1,95 @@
 def mean(array)
-  if(array.length==0)
-    puts "The array is empty"
+  if array.empty?
+    puts 'The array is empty'
   else
-    m=0
-    array.each {|l| m=m+l}
-    puts "Mean is #{(m.to_f/array.length).round(2)}"
+    m = 0
+    array.each { |l| m += l }
+    puts "Mean is #{(m.to_f / array.length).round(2)}"
   end
 end
 
 def mode(array)
-  if(array.length==0)
-    puts "The array is empty"
+  if array.empty?
+    puts 'The array is empty'
   else
-    h = array.each_with_object(Hash.new(0)) {|word, hash| hash[word] += 1}
-    puts "Mode is #{h.key(h.values.max).to_s}"
+    h = array.each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
+    puts "Mode is #{h.key(h.values.max)}"
   end
 end
 
 def median(array)
-  mo=array.length
-  if(mo==0)
-    puts "The array is empty"
+  mo = array.length
+  if mo == 0
+    puts 'The array is empty'
   else
     array.sort! { |a, b| a <=> b }
-    if(mo%2==0)
-      puts "Median is #{(array[mo/2]+array[(mo/2)-1]).to_f/2}"
+    if mo.even?
+      puts "Median is #{(array[mo / 2] + array[(mo / 2) - 1]).to_f / 2}"
     else
-      puts "Median is #{array[mo/2]}"
+      puts "Median is #{array[mo / 2]}"
     end
   end
 end
 
-arr=[]
-op=0
-36.times{ print "-"}
+arr = []
+op = 0
+36.times { print '-' }
 puts "\n|              Problem 2           |"
-while op.to_i!=8 do
-  36.times{ print "-"}
+while op.to_i != 8
+  36.times { print '-' }
   puts "\n|               Menu               |"
-  36.times{ print "-"}
+  36.times { print '-' }
   puts "\n| 1.Add number to array            |"
-  puts "| 2.Print the array                |"
-  puts "| 3.Calculate mean                 |"
-  puts "| 4.Calculate median               |"
-  puts "| 5.Calculate mode                 |"
-  puts "| 6.Calculate mean, mode an median |"
-  puts "| 7.Clear the array                |"
-  puts "| 8.Exit                           |"
-  36.times{ print "-"}
+  puts '| 2.Print the array                |'
+  puts '| 3.Calculate mean                 |'
+  puts '| 4.Calculate median               |'
+  puts '| 5.Calculate mode                 |'
+  puts '| 6.Calculate mean, mode an median |'
+  puts '| 7.Clear the array                |'
+  puts '| 8.Exit                           |'
+  36.times { print '-' }
   puts "\nType the number of the option:"
-  op= gets.chomp
+  op = gets.chomp
 
   case op.to_i
-    when 1
-      puts "Type the number you like to add to the array"
-      num=gets.chomp
-      begin
-        num = Integer(num)
-        arr << num
-        puts "Number added correctly"
-      rescue ArgumentError, TypeError
-        puts "Type a valid number"
-      end
-    when 2
-      if arr.length==0
-        puts "The array is empty"
-      else
-        puts "The array is: #{arr}"
-      end
-    when 3
-      mean(arr)
-    when 4
-      median(arr)
-    when 5
-      mode(arr)
-    when 6
-      mean(arr)
-      median(arr)
-      mode(arr)
-    when 7
-      puts "Are you sure you want to clear the array? (S/N)"
-      q=gets.chomp
-      if(q.downcase=="s")
-        arr.clear
-        puts "Array cleared"
-      else
-        puts "The array was not cleared"
-      end
-    when 8
-      puts "Leaving the program..."
+  when 1
+    puts 'Type the number you like to add to the array'
+    num = gets.chomp
+    begin
+      num = Integer(num)
+      arr << num
+      puts 'Number added correctly'
+    rescue ArgumentError, TypeError
+      puts 'Type a valid number'
+    end
+  when 2
+    if arr.empty?
+      puts 'The array is empty'
     else
-      puts "The option is not valid"
+      puts "The array is: #{arr}"
+    end
+  when 3
+    mean(arr)
+  when 4
+    median(arr)
+  when 5
+    mode(arr)
+  when 6
+    mean(arr)
+    median(arr)
+    mode(arr)
+  when 7
+    puts 'Are you sure you want to clear the array? (S/N)'
+    q = gets.chomp
+    if q.casecmp('s').zero?
+      arr.clear
+      puts 'Array cleared'
+    else
+      puts 'The array was not cleared'
+    end
+  when 8
+    puts 'Leaving the program...'
+  else
+    puts 'The option is not valid'
   end
 end
