cf3b53acc38a7e3a5ccdb0fd770e39fbde304af8
Carlos Medrano <cemg_neto@hotmail.com>
2019-02-13 08:32:07 -0600

    Merge branch 'master' of https://github.com/carlosMedrano27/applaudo-trainee

--- Week2/Ejer1.rb
+++ Week2/Ejer1.rb
@@ -1,10 +1,18 @@
 require 'date'
 require 'time'
+<<<<<<< HEAD
 def goodFileCreation(dates, name)
   File.open("#{name}.csv", 'w') do |csv|
     dates.each_with_index do |val, index|
       if index.odd?
         csv.puts val.to_s
+=======
+def goodFileCreation(dates,name)
+  File.open("#{name}.csv", 'w') do |csv|
+    dates.each_with_index do |val, index|
+      if index.odd?
+        csv.puts "#{val}"
+>>>>>>> bffc603661d49cc72507dd096388a6d9524fcfe7
       else
         csv.print "#{val},"
       end
@@ -12,10 +20,17 @@ def goodFileCreation(dates, name)
   end
 end
 
+<<<<<<< HEAD
 def wrongFileCreation(dates, name)
   File.open("#{name}.csv", 'w') do |csv|
     (0...dates.length).step(3).each do |index|
       csv.puts "#{dates[index]}, #{dates[index + 1]},#{dates[index + 2]}"
+=======
+def wrongFileCreation(dates,name)
+  File.open("#{name}.csv", 'w') do |csv|
+    (0...dates.length).step(3).each do |index|
+        csv.puts "#{dates[index]}, #{dates[index+1]},#{dates[index+2]}"
+>>>>>>> bffc603661d49cc72507dd096388a6d9524fcfe7
     end
   end
 end
@@ -62,5 +77,10 @@ rescue Exception => msg
   puts "Exception Backtrace: #{msg.backtrace}"
  end
 
+<<<<<<< HEAD
 goodFileCreation(goodDate, 'GoodDate')
 wrongFileCreation(badDate, 'BadDate')
+=======
+ goodFileCreation(goodDate,"GoodDate")
+ wrongFileCreation(badDate,"BadDate")
+>>>>>>> bffc603661d49cc72507dd096388a6d9524fcfe7
