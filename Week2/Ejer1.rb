af98abe2728d230498b5a46fc6e14627b1930d20
Carlos Medrano <cemg_neto@hotmail.com>
2019-02-13 18:01:05 -0600

    Added method for print the graph

--- Week2/Ejer1.rb
+++ Week2/Ejer1.rb
@@ -1,83 +1,100 @@
-require 'date'
 require 'time'
-<<<<<<< HEAD
+require 'date'
+@goodDate ||= []
+@badDate ||= []
+class String
+def red;            "\e[31m#{self}\e[0m" end
+def green;          "\e[32m#{self}\e[0m" end
+def brown;          "\e[33m#{self}\e[0m" end
+end
 def goodFileCreation(dates, name)
   File.open("#{name}.csv", 'w') do |csv|
-    dates.each_with_index do |val, index|
-      if index.odd?
-        csv.puts val.to_s
-def goodFileCreation(dates,name)
+    dates.sort_by! { |s| [s.year, s.month, s.day, s.hour, s.min, s.sec] }
+    csv.puts 'Date in EST Zone'
+    dates.each do |val|
+      csv.puts val.to_s
+    end
+    csv.puts "\n\nGRAPHIC:"
+    csv.print "╔"
+    ((@badDate.length/3+ dates.length)*2).times { csv.print '═' }
+    csv.puts "\n║"
+    csv.puts '║  ' + '■ ' * dates.length + " #{dates.length} valid dates"
+    csv.puts "║  "
+    csv.puts '║  ' + '■ ' * (@badDate.length/3) + " #{@badDate.length/3} invalid dates"
+    csv.puts "║  "
+    csv.print "╚"
+    ((@badDate.length/3+ dates.length)*2).times { csv.print '═' }
+  end
+end
+
+def wrongFileCreation(dates, name)
   File.open("#{name}.csv", 'w') do |csv|
-    dates.each_with_index do |val, index|
-      if index.odd?
-        csv.puts "#{val}"
->>>>>>> bffc603661d49cc72507dd096388a6d9524fcfe7
-      else
-        csv.print "#{val},"
+    csv.puts 'Event,Invalid Date,Line Number'
+    (0...dates.length).step(3).each do |index|
+      csv.puts "#{dates[index]},#{dates[index + 1]},#{dates[index + 2]}"
+    end
+  end
+end
+
+def reader
+  fileArray = []
+  @goodDate.clear
+  @badDate.clear
+  c = 1
+  File.open('events.csv', 'r') do |fl|
+    while line = fl.gets
+      fileArray = line.delete("\n").split(',')
+      begin
+        datetime = DateTime.strptime("#{fileArray[1]} CST", '%d/%m/%Y %H:%M:%S %Z')
+        @goodDate << datetime.to_time.getlocal('-05:00')
+      rescue ArgumentError => e
+        @badDate << fileArray[0] << fileArray[1] << c
+      ensure
+        c += 1
       end
     end
   end
-end
 
-<<<<<<< HEAD
-def wrongFileCreation(dates, name)
-  File.open("#{name}.csv", 'w') do |csv|
-    (0...dates.length).step(3).each do |index|
-      csv.puts "#{dates[index]}, #{dates[index + 1]},#{dates[index + 2]}"
-def wrongFileCreation(dates,name)
-  File.open("#{name}.csv", 'w') do |csv|
-    (0...dates.length).step(3).each do |index|
-        csv.puts "#{dates[index]}, #{dates[index+1]},#{dates[index+2]}"
->>>>>>> bffc603661d49cc72507dd096388a6d9524fcfe7
-    end
+  (0...@badDate.length).step(3).each do |index|
+    puts "The file has an invalid date in line #{@badDate[index + 2]}"
   end
-end
-
-begin
-   format = '%d/%m/%Y %H:%M:%S'
-   a = []
-   b = []
-   counter = 0
-   errors = []
-   goodDate = []
-   badDate = []
-   event = ''
-   File.open('events.csv', 'r') do |fl|
-     while line = fl.gets
-       b = line.delete("\n").split(',')
-       (a << b).flatten!
-     end
-   end
-   puts a.length
-   a.each_with_index do |val, index|
-     if index.odd?
-       begin
-         datetime = DateTime.strptime(val, format)
-         a[index] = (Time.parse datetime.to_s) + Time.zone_offset('EST')
-         goodDate << event
-         goodDate << a[index]
-       rescue Exception => e
-         counter += 1
-         errors << ((index + 1) / 2)
-         badDate << event
-         badDate << val
-         badDate << ((index + 1) / 2)
-       end
-     else
-       event = a[index]
-     end
-   end
-
-   puts "The file has #{counter} wrong dates"
-rescue Exception => msg
+rescue Errno::ENOENT => msg
   puts "Exception Class: #{msg.class.name}"
   puts "Exception Message: #{msg.message}"
   puts "Exception Backtrace: #{msg.backtrace}"
- end
+end
 
- goodFileCreation(goodDate,"GoodDate")
- wrongFileCreation(badDate,"BadDate")
-=======
- goodFileCreation(goodDate,"GoodDate")
- wrongFileCreation(badDate,"BadDate")
->>>>>>> bffc603661d49cc72507dd096388a6d9524fcfe7
+def safe(proc)
+  proc.call
+rescue Errno::ENOENT => msg
+  puts "Exception Class: #{msg.class.name}"
+  puts "Exception Message: #{msg.message}"
+  puts "Exception Backtrace: #{msg.backtrace}"
+rescue NameError => name
+  puts "Exception Class: #{name.class.name}"
+  puts "Exception Message: #{name.message}"
+  puts "Exception Backtrace: #{name.backtrace}"
+end
+
+def graph(vDates, iDates)
+  puts "\n\nGRAPHIC:"
+  print ("╔").brown
+  ((vDates + iDates)*2).times { print ('═').brown }
+  puts ("\n║").brown
+  print ('║  ').brown
+  print ('■ ' * vDates).green
+  puts " #{vDates} valid dates"
+  puts ("║  ").brown
+  print ('║  ').brown
+  print ('■ ' * iDates).red
+  puts " #{iDates} invalid dates"
+  puts ('║  ').brown
+  print ("╚").brown
+  ((vDates + iDates)*2).times { print ('═').brown }
+  puts
+end
+
+reader
+safe(proc { goodFileCreation(@goodDate, 'GoodDate') })
+safe(proc { wrongFileCreation(@badDate, 'BadDate') })
+graph(@goodDate.length, (@badDate.length / 3))
