5c95027273824bd745e9c72beffcdfc08a64c88f
Carlos Medrano <cemg_neto@hotmail.com>
2019-02-14 10:20:42 -0600

    Thursday

--- Week2/Ejer1.rb
+++ Week2/Ejer1.rb
@@ -2,10 +2,19 @@ require 'time'
 require 'date'
 @goodDate ||= []
 @badDate ||= []
+@Total ||= 0
+@secondline ||=[]
+@number ||=[]
 class String
-def red;            "\e[31m#{self}\e[0m" end
-def green;          "\e[32m#{self}\e[0m" end
-def brown;          "\e[33m#{self}\e[0m" end
+  def red
+    "\e[31m#{self}\e[0m"
+  end
+  def green
+    "\e[32m#{self}\e[0m"
+  end
+  def brown
+    "\e[33m#{self}\e[0m"
+  end
 end
 def goodFileCreation(dates, name)
   File.open("#{name}.csv", 'w') do |csv|
@@ -15,15 +24,31 @@ def goodFileCreation(dates, name)
       csv.puts val.to_s
     end
     csv.puts "\n\nGRAPHIC:"
-    csv.print "╔"
-    ((@badDate.length/3+ dates.length)*2).times { csv.print '═' }
+    csv.print '╔'
+    (60).times { csv.print '═' }
+    csv.print "╗"
     csv.puts "\n║"
-    csv.puts '║  ' + '■ ' * dates.length + " #{dates.length} valid dates"
-    csv.puts "║  "
-    csv.puts '║  ' + '■ ' * (@badDate.length/3) + " #{@badDate.length/3} invalid dates"
-    csv.puts "║  "
-    csv.print "╚"
-    ((@badDate.length/3+ dates.length)*2).times { csv.print '═' }
+    csv.puts '║  ' + '■' *( dates.length*100/@Total/2) + " #{dates.length*100/@Total}% valid dates"
+    csv.puts '║  '
+    csv.puts '║  ' + '■' *(@badDate.length / 3*100/@Total/2) + " #{@badDate.length / 3*100/@Total}% invalid dates"
+    csv.puts '║  '
+    csv.print '║  '
+    csv.puts "-"*52 +"%"
+    csv.print '║  '
+    (0..100).step(4).each do |index|
+      @number = index.to_s.split('')
+      @secondline << @number[1]
+      csv.print "#{@number[0]} "
+    end
+    csv.print "\n║     "
+    @secondline.each do |val|
+      csv.print "#{val} "
+    end
+    csv.print "\n║"
+    csv.print " "*52 +"0"
+    csv.print "\n╚"
+    (60).times { csv.print '═' }
+    csv.print "╝"
   end
 end
 
@@ -36,12 +61,12 @@ def wrongFileCreation(dates, name)
   end
 end
 
-def reader
+def reader(name)
   fileArray = []
   @goodDate.clear
   @badDate.clear
   c = 1
-  File.open('events.csv', 'r') do |fl|
+  File.open("#{name}.csv", 'r') do |fl|
     while line = fl.gets
       fileArray = line.delete("\n").split(',')
       begin
@@ -78,23 +103,59 @@ end
 
 def graph(vDates, iDates)
   puts "\n\nGRAPHIC:"
-  print ("╔").brown
-  ((vDates + iDates)*2).times { print ('═').brown }
-  puts ("\n║").brown
-  print ('║  ').brown
-  print ('■ ' * vDates).green
-  puts " #{vDates} valid dates"
-  puts ("║  ").brown
-  print ('║  ').brown
-  print ('■ ' * iDates).red
-  puts " #{iDates} invalid dates"
-  puts ('║  ').brown
-  print ("╚").brown
-  ((vDates + iDates)*2).times { print ('═').brown }
+  print '╔'.brown
+  (60).times { print '═'.brown }
+  print "╗".brown
+  puts "\n║".brown
+  print '║  '.brown
+  print ('■' *(vDates/2)).green
+  puts " #{vDates}% valid dates"
+  puts '║  '.brown
+  print '║  '.brown
+  print ('■' *(iDates/2)).red
+  puts " #{iDates}% invalid dates"
+  puts '║  '.brown
+  print '║  '.brown
+  puts "-"*52 +"%"
+  print '║  '.brown
+  (0..100).step(4).each do |index|
+    @number = index.to_s.split('')
+    print "#{@number[0]} "
+  end
+  print "\n║     ".brown
+  @secondline.each do |val|
+    print "#{val} "
+  end
+  print "\n║".brown
+  print " "*52 +"0"
+  print "\n╚".brown
+  (60).times { print '═'.brown }
+  print "╝".brown
   puts
 end
 
-reader
-safe(proc { goodFileCreation(@goodDate, 'GoodDate') })
-safe(proc { wrongFileCreation(@badDate, 'BadDate') })
-graph(@goodDate.length, (@badDate.length / 3))
+input = ''
+goodFile = ''
+badFile = ''
+36.times { print '-' }
+puts "\n|              Dates               |"
+while goodFile == badFile || input == badFile || input == badFile
+  36.times { print '-' }
+  puts "\nType the name of the input file without the .csv:"
+  input = gets.chomp
+  puts 'Type the name of the file for the valid dates without the .csv:'
+  goodFile = gets.chomp
+  puts 'Type the name of the file for the invalid dates without the .csv:'
+  badFile = gets.chomp
+  next unless goodFile == badFile || input == badFile || input == badFile
+  sleep(1)
+  system('clear')
+  puts 'The name of the 3 files must be different '
+end
+sleep(1)
+system('clear')
+reader(input)
+@Total=(@badDate.length/3 + @goodDate.length).to_i
+safe(proc { goodFileCreation(@goodDate, goodFile) })
+safe(proc { wrongFileCreation(@badDate, badFile) })
+graph(@goodDate.length*100/@Total, (@badDate.length / 3)*100/@Total)
---
+++ Week2/README.md
@@ -0,0 +1,46 @@
+## Week 2 Evaluation
+
+
+First shows the header like this
+```
+------------------------------------
+|              Dates               |
+------------------------------------
+```
+then proceed to ask the person for the name of the file where all the dates are,
+after that  ask the person for the of the file that will contain the valid dates
+and the name of the file for the invalid dates, then check, if the names are the same
+proceed to ask them again
+
+**Reader Method**
+
+This reads the file and validates if is a valid date or not, the date use the format '%d/%m/%Y %H:%M:%S' but I added the `CST` to especify the time zone where El Salvador is, and then proceed to get the time of `EST` with `getlocal('-05:00')`, -05:00 is for the EST zone, if this process is successful  then added the date to an array of god dates if not then added the date, the event and the line number to an array of bad dates, later proceeds to print a description of the lines where are invalid dates.
+
+ **goodFileCreation Method**
+ This method gets the array of the valid dates and the name of the output file,
+ is responsible for ordering the array by date and to write the file and a graphic
+ like this were the percentages are written vertically:
+
+```
+GRAPHIC:
+
+╔════════════════════════════════════════════════════════════╗
+║
+║  ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ 78% valid dates
+║  
+║  ■■■■■■■■■■■ 22% invalid dates
+║  
+║  ----------------------------------------------------%
+║  0 4 8 1 1 2 2 2 3 3 4 4 4 5 5 6 6 6 7 7 8 8 8 9 9 1
+║        2 6 0 4 8 2 6 0 4 8 2 6 0 4 8 2 6 0 4 8 2 6 0
+║                                                    0
+╚════════════════════════════════════════════════════════════╝
+```
+ **wrongFileCreation Method**
+ This method gets the array of the invalid dates and the name of the output file,
+ is responsible for write the file with the values of the array (event,Date,Line number)
+
+ The methods `goodFileCreation` and `wrongFileCreation` are call using a proc call *safe* that rescue the possible exceptions that may happen
+
+ **graph Method**
+ This method gets the number of valid dates and the number of invalid dates and then print a graphic like the one in the file of the valid dates
