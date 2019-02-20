ec8c294ad4b4e38d3577f5c35229b14c0b274ad2
Carlos Medrano <cemg_neto@hotmail.com>
2019-02-20 16:43:52 -0600

    Added 2 decimals to prices

--- Week3/store.rb
+++ Week3/store.rb
@@ -20,7 +20,7 @@ class Store
       puts 'The Id of the vehicle is repeated'
     else
       @vehicles.push vec
-      puts 'Vehicle added'
+      puts 'Vehicle added correctly'
     end
   end
 
@@ -41,7 +41,7 @@ class Store
       puts "\t\tColor: #{l.color}"
       puts "\t\ttBrand: #{l.brand}"
       puts "\t\tWheels: #{l.wheels}"
-      puts "\t\tPrice: $#{l.price}"
+      puts "\t\tPrice: $#{sprintf('%.2f', l.price)}"
       36.times { print '-' }
       puts ''
     end
@@ -53,7 +53,7 @@ class Store
     36.times { print '-' }
     puts ''
     Extras.extras_array.each_with_index do |l, index|
-      puts "|\t#{index + 1}.- #{l.extra} ($#{l.price})"
+      puts "|\t#{index + 1}.- #{l.extra} ($#{sprintf('%.2f', l.price)})"
     end
     36.times { print '-' }
     puts
@@ -61,26 +61,27 @@ class Store
 
   def print_details(vehicle, extras = [])
     total = 0.0
-    puts 'Detail:'
-    puts "\t\tQuote for the car:  #{vehicle.id}"
-    puts "\t\tFeatures"
-    puts "\t\tColor: #{vehicle.color}"
-    puts "\t\tBrand: #{vehicle.brand}"
-    puts "\t\tWheels: #{vehicle.wheels}"
-    puts "\t\tPrice: $#{vehicle.price}"
+    36.times { print '-' }
+    puts "\nDetail:"
+    puts "\tQuote for the car:  #{vehicle.id}"
+    puts "\tFeatures"
+    puts "\tColor: #{vehicle.color}"
+    puts "\tBrand: #{vehicle.brand}"
+    puts "\tWheels: #{vehicle.wheels}"
+    puts "\tPrice: $#{sprintf('%.2f', vehicle.price)}"
     total += vehicle.price
     40.times { print '-' }
     puts ''
     if extras.length>0
       extras.each do |l|
-        puts "\t\t#{l.extra} ($#{l.price})"
+        puts "\t#{l.extra} ($#{sprintf('%.2f', l.price)})"
         total += l.price
       end
     else
-      puts "\t\tNo extras selected"
+      puts "\tNo extras selected"
     end
 
     40.times { print '-' }
-    puts "\n\t\tTotal: $#{total}"
+    puts "\n\tTotal: $#{sprintf('%.2f', total)}"
   end
 end
