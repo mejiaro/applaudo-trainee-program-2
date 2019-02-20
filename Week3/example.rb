c538c13247616fc9703d71f754e146b7a9bc8099
Carlos Medrano <cemg_neto@hotmail.com>
2019-02-20 10:58:57 -0600

    check if methods of add an delete works

--- Week3/example.rb
+++ Week3/example.rb
@@ -1,20 +1,47 @@
 require_relative 'vehicle'
 require_relative 'car'
 require_relative 'truck'
+require_relative 'store'
 
 a=[]
 truck=[]
 car=[]
-a.replace(Vehicle.array)
-a.each do |l|
+puts "Number of vehicles #{Vehicle.vehicles_array.length}"
+Vehicle.vehicles_array.each do |l|
+  puts "ID:  #{l.id}"
+  puts "Brand: #{l.brand}"
+  puts "Model: #{l.model}"
+  puts "Color: #{l.color}"
+  puts "Price: $#{l.price}"
+end
+
+
+store = Store.new(Vehicle.vehicles_array)
+c = Car.new("black", "Nissan", "Versa", 10000)
+c.wheels
+store.add_vehicle(c)
+
+store.vehicles.each do |l|
+  puts "ID:  #{l.id}"
+  puts "Brand: #{l.brand}"
+  puts "Model: #{l.model}"
+  puts "Color: #{l.color}"
+  puts "Price: $#{l.price}"
+end
+
+store.delete_vehicle(Vehicle.vehicles_array[4])
+store.vehicles.each do |l|
   if l.wheels > 4
-    truck.push a
+    puts "Truck"
   else
-    car.push a
+    puts "Car"
   end
+  puts "ID:  #{l.id}"
+  puts "Brand: #{l.brand}"
+  puts "Model: #{l.model}"
+  puts "Color: #{l.color}"
+  puts "Price: $#{l.price}"
+  puts "Wheels: #{l.wheels}"
+  puts "--------------------\n"
 end
-puts truck.length
-truck.each do |l|
-  puts "ID #{l.id}"
-  puts "Color #{l.color}"
-end
+puts store.vehicles.length
